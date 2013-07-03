96d39a4f0fefb8f860bc1281c35cf8095eaa7d51
Dylan Lingelbach <dylan.lingelbach@gmail.com>
2013-07-03 14:20:55 -0500

    Fix method definition

--- lib/mixpanel/uri.rb
+++ lib/mixpanel/uri.rb
@@ -23,14 +23,14 @@ module Mixpanel
     rescue OpenURI::HTTPError => error
       raise HTTPError, JSON.parse(error.io.read)['error']
     end
-  end
 
-  def self.post(uri)
-    uri = ::URI.parse(uri)
-    request = Net::HTTP::Post.new(uri.request_uri)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    http.request(request)
+    def self.post(uri)
+      uri = ::URI.parse(uri)
+      request = Net::HTTP::Post.new(uri.request_uri)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.request(request)
+    end
   end
 end
